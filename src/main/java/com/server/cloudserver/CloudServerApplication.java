9387a25d5385bc8adc44a14f6792d159edad3d91
iranmzk <iranmpc93@gmail.com>
2022-10-14 10:17:39 -0300

    Updated.

--- src/main/java/com/server/cloudserver/CloudServerApplication.java
+++ src/main/java/com/server/cloudserver/CloudServerApplication.java
@@ -10,6 +10,7 @@ public class CloudServerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CloudServerApplication.class, args);
+        System.out.println("Server is on");
     }
 
 }
